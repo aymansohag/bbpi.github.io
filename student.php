bd83350e9dc2a135ed2e6b74f226736e58199425
aymansohag <47294719+aymansohag@users.noreply.github.com>
2019-02-03 19:56:13 +0600

    3rd website

---
+++ student.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>B.Baria Polytechnic Institute</title> 
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous">
+</head>
+<body>
+	<div class="wrapper fix">
+		<header>
+			<div class="header fix">
+				<div class="leftheader fix floatleft">
+					<a href="#"><img src="img/logo.png"></a>
+				</div>
+				<div class="inheader fix floatleft">
+				    <h1>Brahmanbaria Polytechnic Institute</h1>
+				    <h2>Islampur,Bijoynogor</h2>
+				    <h3>" শিক্ষা নিয়ে গড়ব দেশ শেখহাসিনার বাংলাদেশ "</h2>
+				</div>
+				<div class="rightheader fix floatleft">
+					<a href="student.html"><button>Sign In</button></a>
+		        </div>
+		    </div>
+		</header>
+		<div class="main width fix">
+			<div class="animation">
+	        		<h1>-----Ragistration Now-----</h1>
+	        </div>
+			<nav>
+				<div class="mainmenu width">  <!------Main menu start-------------->
+					<ul class="menu">
+						<li><a href="index.html" style="border-left: none;" >Home</a></li>
+						<li><a href="aboutbbpi.html">About bbpi</a></li>
+						<li><a href="#" id="activ">Contuct Us</a>
+							<ul>
+								<li><a href="principal.html">Principal</a></li>
+							    <li><a href="biceprincipal.html">Bice Principal</a></li>	
+							     <li><a href="registersir.html">Regester sir</a></li>	
+							</ul>
+						</li>
+						<li><a href="#">Depertment</a>
+							<ul>
+								<li><a href="emt.html">Electromedical</a></li>
+								 <li><a href=rat.html>Refrigeretor</a></li>		
+								<li><a href="cmt.html">Computer</a></li>
+							    <li><a href="aidt.html">Arcitecher</a></li>	
+							</ul>
+						</li>
+						<li><a href="#">Academic</a>
+							<ul>
+								<li><a href="result.html">Result</a></li>	
+								<li><a href="img/calender.pdf">Academic Calender</a></li>
+							    <li><a href="img/calender.pdf">Routine 1st shift</a></li>	
+							    <li><a href="img/calender.pdf">Routine 2nd shift</a></li>	
+							    <li><a href="img/calender.pdf">Probidha</a></li>	
+							</ul>
+						</li>
+						<li><a href="#">Stipend</a>
+							<ul>
+								<li><a href="#">Govt. stipend</a></li>
+							    <li><a href="#">STEP stipend</a></li>	
+							</ul>
+						</li>						
+						<li><a href="#">Placement</a>
+							<ul>
+								<li><a href="#">Placement status</a></li>
+							    <li><a href="#">Placment cell</a></li>	
+							</ul>
+						</li>	
+						<li><a href="#">Bload Bank</a></li>
+						<li><a href="#">Member Panel</a></li>
+						<li><a href="#" style="border-right: none;">Adminastration</a></li>
+					</ul>
+				</div>          <!------Main menu end-------------->
+			</nav>
+	        <content>            <!------Content part Start-------------->
+	        	<div class="maincontent fix width">
+	        		<div class="submaincontent fix">
+	        			<div class="principal fix width_2" style="background: wheat; border-radius:20px;">	
+             				<h1 class="p_h1">Enter your infonmation and massage then submit</h1>
+             				<div class="p_form fix" style="min-height: 800px;">
+             					<div class="f_header fix">
+             						<table>
+             						<tr>
+             							<td class="td">Roll:</td>
+             						    <td><input type="number" name="roll" placeholder="Enter your roll"></td>
+             						</tr>
+             						<tr>
+             							<td class="td">Name:</td>
+             						    <td><input type="text" name="name" placeholder="Enter your name"></td>
+             						</tr>
+             						<tr>
+             							<td class="td">Email:</td>
+             						    <td><input type="email" name="email" placeholder="Enter your email"></td>
+             						</tr>
+             						<tr>
+             							<td class="td">Password:</td>
+             						    <td><input type="text" name="password" placeholder="Enter Your password"></td>
+             						</tr>
+             						<tr>
+             							<td class="td">Re Enter Password:</td>
+             						    <td><input type="text" name="rpassword" placeholder="Re Enter Password"></td>
+             						</tr>
+             						<tr>
+             							<td class="td">Birth Date:</td>
+             						    <td><input type="date" name="date" placeholder="Re Enter Password"></td>
+             						</tr>
+             						<tr>
+             							<td class="td">Gender:</td>
+             						    <td><input type="radio" name="gender">Male</td>   
+             						</tr>
+             						<tr>		
+             						    <td></td>
+             						    <td><input type="radio" name="gender">Female</td>
+             						</tr>
+             						<tr>
+             							<td class="td">Write Massage</td>
+             						    <td><textarea rows="10" cols="40" placeholder="Writ your massage"></textarea></td>
+             						</tr>
+             					</table>
+             					<div class="f_footer fix">
+             						<button class="button" type="submit">
+             							Submit Now
+             						</button>
+             					</div>
+             					</div>
+             					
+             				</div>
+	        				
+	        			</div>
+	        			
+	        		</div>
+	        		
+	        	</div>
+	        </content>     <!------Content part End-------------->
+	        <div class="information fix">
+	        	<div class="inf fix">
+	        		<h1>---------Information Us---------</h1>
+	        	</div>
+	        	<div class="iimg fix">
+	        		<a href="#" title="Clic here to red information"><i class="fas fa-user-graduate"></i></a>
+	        		<h2 title="Clic here to read information"><a href="">Student Information</a></h2>
+	        	</div>
+	        	<div class="iimg fix">
+	        		<a href="#" title="Clic here to red information"><i class="fas fa-chalkboard-teacher"></i></a>
+	        		<h2 title="Clic here to read information"><a href="">Teacher Information</a></h2>
+	        	</div>
+	        	<div class="iimg fix">
+	        		<a href="#"><i class="fas fa-users-cog" title="Clic here to red information"></i></a>
+	        		<h2 title="Clic here to read information"><a href="">Staff Information</a></h2>
+	        	</div>
+	            <div class="iimg fix">
+	            	<a href="#"><i class="fas fa-database" title="Clic here to red information"></i></a>
+	            	<h2 title="Clic here to read information"><a href="">Placement Cell</a></h2>
+	            </div>
+	        </div>
+		</div> <!------Main part End-------------->
+		<footer>
+			<div class="fotter fix">
+				<div class="fotterwidth fix width">
+					<div class="f_cl1 fix floatleft">
+						<h1>Brahmanbaria Polytechnic Institute</h1>
+						<h2>Islampur,Bojoynagor,Brahmanbaria</h2>
+						<h2>Postalcode:000000</h2>
+						<h2>Tel: xxxx xxxxxxxxxx</h2>
+						<h2>Fax: xxxx xxxxxxxxxx</h2>
+						<h2>Email:brahmanbariapoly@gmail.com</h2>
+						 <a href="#" title="Click to followed Facebook">
+						 	<div class="an">
+						 	   <h2 style="border-bottom: none;" class="followed">--Followed Facebook--</h2>
+						   </div>
+					    </a>
+				    </div>
+				    <div class="f-cl2 fix floatleft">
+        			    <ul  class="f-cl2_ul">
+        					<a href="#"><li>Student Activities</li></a>
+        					<a href="#"><li>Student Affairs</li></a>
+        					<a href="#"><li>Student Development</li></a>
+        					<a href="#"><li>Student Financial Services</li></a>
+        					<a href="#"><li>Student Group Directory</li></a>
+        					<a href="#"><li>Student Life</li></a>
+        				</ul>
+
+				    </div>
+				    <div class="f-cl3 fix floatleft">
+        			    <ul>
+        			    	<a href="#"><li>Bd Jobs</li></a>
+        					<a href="#"><li>DUTE</li></a>
+        					<a href="#"><li>SUST</li></a>
+        					<a href="#"><li>CUTE</li></a>
+        					<a href="#"><li>KUTE</li></a>
+        					<a href="#"><li>BUTE</li></a>
+        					<a href="#"><li>RUTE</li></a>	
+        				</ul>
+				    </div>
+				</div>
+				<div class="copy width fix">
+					<div class="ccl1 fix floatleft">
+						<p>&copy; Copywrite B.Baria polytechnic Istitute</p>
+					</div>
+					<div class="ccl2 floatright fix">
+						<h1>Design by:Sohag Ahmed Joy</h1>
+						<h1><a href="#">--Contuct--</a></h1>
+					</div>
+			   </div>
+			</div>
+		</footer>
+	</div>
+
+</body>
+</html>
